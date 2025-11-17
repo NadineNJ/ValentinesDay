4f291522f8d00c9013dc5d11e2abbd0aa2a248f7
NadineNJ <151051850+NadineNJ@users.noreply.github.com>
2025-11-17 10:45:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const wrapper = document.querySelector('.wrapper');
+const question =document.querySelector('.question');
+const gif = document.querySelector('.gif');
+const yes8tn = document.querySelector('.yes-btn');
+const no8tn = document.querySelector('.no-btn');
+const sound = document.getElementById("yaySound");
+
+
+yes8tn.addEventListener("click", () => {
+    question.innerHTML = "yeeey!!!! 🎉";
+    gif.src =
+      "https://media.tenor.com/gpUOSzd4uVQAAAAj/happy-cat.gif" 
+        sound.play();   
+
+
+});
+
+no8tn.addEventListener('mouseover', () => {
+    const noBtnRect = no8tn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    no8tn.style.left = randomX + 'px';
+    no8tn.style.top = randomY + 'px' ;
+})
